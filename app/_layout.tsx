cce4b2e18da10be09e8f2ed036ff2546d0c2eae1
ChaudaryHammad <ch.hammadbrw01@gmail.com>
2024-10-11 12:09:43 +0500

    batman

---
+++ app/_layout.tsx
@@ -0,0 +1,41 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function TabLayout() {
+
+
+  return (
+   <>
+
+
+   <StatusBar style="dark" />
+
+   
+   
+   <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' ,headerShown:false}}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'For you',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+        <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+    </Tabs>
+   </>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text} from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const foryou = () => {
+  return (
+    <SafeAreaView>
+      
+      <Text>foryou</Text>
+    </SafeAreaView>
+  )
+}
+
+export default foryou
